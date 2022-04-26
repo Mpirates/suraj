b5555b88a56522b74ace2432287d6da82f5f72de
hackgit12 <98569431+hackgit12@users.noreply.github.com>
2022-04-26 16:10:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -407,12 +407,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ɪɴ yᴏᴜʀ ɢʀᴏᴜᴩ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
             InlineKeyboardButton('🔍 ꜱᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🕹️ɢʀᴏᴜᴩ', url='https://t.me/FILIMPIRATESGROUP')
+            InlineKeyboardButton('🕹️ɢʀᴏᴜᴩ', url='https://t.me/FilmPiratesGroup')
         ], [
             InlineKeyboardButton('⚙️ʜᴇʟᴩ', callback_data='help'),
             InlineKeyboardButton('🥵ᴀʙᴏᴜᴛ', callback_data='about')
         ], [
-            InlineKeyboardButton('📢ᴄʜᴀɴɴᴇʟ', url='https://t.me/FPHDMOVE')
+            InlineKeyboardButton('📢ᴄʜᴀɴɴᴇʟ', url='https://t.me/FilmPiratesOfficial')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
