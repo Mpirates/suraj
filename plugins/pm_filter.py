6944abb9fc0fc17a3cfdf3794b054b3f87bf350d
hackgit12 <98569431+hackgit12@users.noreply.github.com>
2022-04-28 16:35:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -133,9 +133,8 @@ async def next_page(bot, query):
     await asyncio.sleep(2100)
     await hehe.delete()
     await message.delete()
-   
-        else:
-            k = await query.message.edit('⚙️Hey {message.from_user.mention},ꜰɪʟᴛᴇʀ ꜰᴏʀ {query} ᴄʟᴏꜱᴇʀ🗑️')
+else:
+    k = await query.message.edit('⚙️Hey {message.from_user.mention},ꜰɪʟᴛᴇʀ ꜰᴏʀ {query} ᴄʟᴏꜱᴇʀ🗑️')
 
 @Client.on_callback_query(filters.regex(r"^spolling"))
 async def advantage_spoll_choker(bot, query):
