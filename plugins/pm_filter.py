af7d40a6a62c2582ee7c7da3b1dfc80ac65f4d45
Mpirates <104631217+Mpirates@users.noreply.github.com>
2022-04-30 21:46:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -97,7 +97,7 @@ async def next_page(bot, query):
         off_set = offset - 10
     if n_offset == 0:
         btn.append(
-            [InlineKeyboardButton("🚶‍♂️ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
+            [InlineKeyboardButton("« Back", callback_data=f"next_{req}_{key}_{off_set}"),
              InlineKeyboardButton(text=f"Check PM!", url=f"https://t.me/{temp.U_NAME}"),
              InlineKeyboardButton(f"📃 {round(int(offset) / 10) + 1} / {round(total / 10)}",
                                   callback_data="pages")]
@@ -110,7 +110,7 @@ async def next_page(bot, query):
     else:
         btn.append(
             [
-                InlineKeyboardButton("🚶‍♂️ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
+                InlineKeyboardButton("« Back", callback_data=f"next_{req}_{key}_{off_set}"),
                 InlineKeyboardButton(f"🗓 {round(int(offset) / 10) + 1} / {round(total / 10)}", callback_data="pages"),
                 InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{req}_{key}_{n_offset}")]
             )
