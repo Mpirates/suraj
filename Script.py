2058b0a827cd309f6b839683118120e296b98030
Mrpirates <104631217+Mpirates@users.noreply.github.com>
2022-05-01 11:50:00 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -362,8 +362,8 @@ Name - {}"""
     STATUS_TXT = """* 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
 * 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
 * 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-* 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝙱
-* 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝙱"""
+* 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code>.
+* 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code>."""
 
     ZOMBIES_TXT = """Help: <b>Zombies</b>
 
